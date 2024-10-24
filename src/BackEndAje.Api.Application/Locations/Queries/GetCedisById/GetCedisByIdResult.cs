500396bdc0b8a20d967cdd3bbbfe780f466fec40
Erik Abanto <90631550+ErikDevCode@users.noreply.github.com>
2024-10-23 19:13:14 -0500

    listado Cedi by Id

---
+++ src/BackEndAje.Api.Application/Locations/Queries/GetCedisById/GetCedisByIdResult.cs
@@ -0,0 +1,9 @@
+namespace BackEndAje.Api.Application.Locations.Queries.GetCedisById
+{
+    public class GetCedisByIdResult
+    {
+        public int Id { get; set; }
+
+        public string CediName { get; set; }
+    }
+}
--- src/BackEndAje.Api.Application/Mappers/LocationProfile.cs
+++ src/BackEndAje.Api.Application/Mappers/LocationProfile.cs
@@ -1,6 +1,7 @@
 namespace BackEndAje.Api.Application.Mappers
 {
     using AutoMapper;
+    using BackEndAje.Api.Application.Locations.Queries.GetCedisById;
     using BackEndAje.Api.Application.Locations.Queries.GetCedisByRegionId;
     using BackEndAje.Api.Application.Locations.Queries.GetRegions;
     using BackEndAje.Api.Application.Locations.Queries.GetZoneByCediId;
@@ -15,6 +16,10 @@ namespace BackEndAje.Api.Application.Mappers
             this.CreateMap<Cedi, GetCedisByRegionIdResult>();
 
             this.CreateMap<Zone, GetZoneByCediIdResult>();
+
+            this.CreateMap<Cedi, GetCedisByIdResult>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.CediId))
+                .ForMember(dest => dest.CediName, opt => opt.MapFrom(src => src.CediName));
         }
     }
 }
